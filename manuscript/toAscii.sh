5719fb5abf598ea1e98dda1245abd6cfec7805ab
Guy Harrison <guy@southbanksoftware.com>
2021-04-14 18:31:28 +1000

    Chapter 2 formatting

--- manuscript/toAscii.sh
+++ manuscript/toAscii.sh
@@ -2,7 +2,7 @@ INPUT_DOCX=$1
 OUTPUT_DOC=`echo $INPUT_DOCX|cut -f1 -d'.'` 
 OUTPUT_DOC="${OUTPUT_DOC}.asciidoc"
 echo $OUTPUT_DOC
-pandoc --to asciidoc --from docx  $INPUT_DOCX >$OUTPUT_DOC
+pandoc --to asciidoc --from docx  "$INPUT_DOCX" >"$OUTPUT_DOC"
 
 # On the Mac, this will open the generated file in MS Word
  
